0ed840aeb7d8a759feaf7aeae74749db4dec2c97
Samama Usman <samop251@gmail.com>
2025-03-18 19:19:37 +0500

    Added the vercel.json file

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["https://your-frontend-domain.com", "http://localhost:3000"],
+    origin: ["*"],
     methods: ["GET", "POST"],
   })
 );
